9550788b6bbb031ec3c112e3c81ee47d96311b1f
Ramon Ribeiro <rhpr@cesar.org.br>
2019-09-10 14:19:17 -0300

    Authenticate after registration

--- src/msg.c
+++ src/msg.c
@@ -1290,7 +1290,7 @@ static void on_device_added(const char *device_id, const char *token,
 	knot_msg_credential msg;
 	ssize_t olen, osent;
 	void *opdu;
-	int err;
+	int err, result, proto_sock;
 
 	if (!session) {
 		hal_log_dbg("Session not found");
@@ -1323,8 +1323,6 @@ static void on_device_added(const char *device_id, const char *token,
 	session->uuid = l_strdup(mydevice->uuid);
 	session->token = l_strdup(token);
 
-	// TODO: Authenticate device on cloud
-
 	memset(&msg, 0, sizeof(msg));
 	msg_credential_create(&msg, mydevice->uuid, token);
 
@@ -1338,6 +1336,19 @@ static void on_device_added(const char *device_id, const char *token,
 		hal_log_error("[session %p] Can't send register response %s(%d)"
 			      , session, strerror(err), err);
 	}
+
+	proto_sock = l_io_get_fd(session->proto_channel);
+	result = proto_signin(proto_sock, session->uuid, token,
+			      property_changed, L_INT_TO_PTR(session->node_fd));
+	if (result != 0) {
+		l_free(session->uuid);
+		l_free(session->token);
+		session->uuid = NULL;
+		session->token = NULL;
+		return;
+	}
+
+	session->rollback = 0; /* Rollback disabled */
 }
 
 /*
