b1d5a4eb82291c565b7edde9560d806b59684feb
Ramon Ribeiro <rhpr@cesar.org.br>
2019-09-13 17:49:09 -0300

    Refactor cloud_receive_msg

--- src/cloud.c
+++ src/cloud.c
@@ -61,15 +61,21 @@
 #define AMQP_CMD_DEVICE_REGISTER "device.register"
 #define AMQP_CMD_DEVICE_LIST "device.cmd.list"
 
-struct cloud_callbacks {
-	cloud_downstream_cb_t update_cb;
-	cloud_downstream_cb_t request_cb;
-	cloud_device_removed_cb_t removed_cb;
-	cloud_device_added_cb_t added_cb;
-	cloud_devices_cb_t listed_cb;
+
+enum cloud_events {
+	update_evt,
+	request_evt,
+	removed_evt,
+	added_evt,
+	listed_evt
 };
 
-static struct cloud_callbacks cloud_cbs;
+static struct l_hashmap *map_event_to_cb;
+
+struct event_handler {
+	void *cb;
+	int event_id;
+};
 
 static const char *get_token_from_jobj(json_object *jso)
 {
@@ -84,43 +90,47 @@ static const char *get_token_from_jobj(json_object *jso)
 	return json_object_get_string(jobjkey);
 }
 
+static const char *get_id_from_json_str(json_object *jso)
+{
+	json_object *jobjkey;
+
+	if (!json_object_object_get_ex(jso, "id", &jobjkey))
+		return false;
+
+	if (json_object_get_type(jobjkey) != json_type_string)
+		return false;
+
+	return json_object_get_string(jobjkey);
+}
+
 static bool cloud_receive_message(const char *exchange,
 				  const char *routing_key,
 				  const char *body, void *user_data)
 {
 	json_object *jso;
-	json_object *id_json;
 	struct l_queue *list;
 	const char *id, *token;
+	struct event_handler *handler;
+	cloud_downstream_cb_t update_cb;
+	cloud_device_removed_cb_t removed_cb;
+	cloud_device_added_cb_t added_cb;
+	cloud_devices_cb_t listed_cb;
 
 	if (strcmp(AMQP_EXCHANGE_FOG, exchange) != 0)
 		return true;
+
+	handler = l_hashmap_lookup(map_event_to_cb, routing_key);
+	if (!handler->cb)
+		return false;
 
 	jso = json_tokener_parse(body);
 	if (!jso)
 		return false;
 
-	if (cloud_cbs.listed_cb != NULL &&
-	    strcmp(AMQP_EVENT_DEVICE_LIST, routing_key) == 0) {
-		list = parser_mydevices_to_list(body);
-		if (!l_queue_isempty(list))
-			cloud_cbs.listed_cb(list, user_data);
-
-		l_queue_destroy(list, NULL);
-		cloud_cbs.listed_cb = NULL;
-		goto done;
-	}
-
-	if (!json_object_object_get_ex(jso, "id", &id_json))
-		return false;
-
-	if (json_object_get_type(id_json) != json_type_string)
-		return false;
-
-	id = json_object_get_string(id_json);
-
-	if (cloud_cbs.update_cb != NULL &&
-	    strcmp(AMQP_EVENT_DATA_UPDATE, routing_key) == 0) {
+	switch (handler->event_id) {
+	case update_evt:
+		update_cb = handler->cb;
+		id = get_id_from_json_str(jso);
 		list = parser_update_to_list(jso);
 		if (list == NULL) {
 			hal_log_error("Error on parse json object");
@@ -128,31 +138,41 @@ static bool cloud_receive_message(const char *exchange,
 			return false;
 		}
 
-		cloud_cbs.update_cb(id, list, user_data);
+		update_cb(id, list, user_data);
 		l_queue_destroy(list, l_free);
-	}
-
-	if (cloud_cbs.request_cb != NULL &&
-	    strcmp(AMQP_EVENT_DATA_REQUEST, routing_key) == 0) {
+		break;
+	case request_evt:
 		return true;
-		/* Call cloud_cbs.request_cb */
-	}
-
-	if (cloud_cbs.removed_cb != NULL &&
-	    strcmp(AMQP_EVENT_DEVICE_UNREGISTERED, routing_key) == 0) {
-		cloud_cbs.removed_cb(id, user_data);
-	}
-
-	if (cloud_cbs.added_cb != NULL &&
-	    strcmp(AMQP_EVENT_DEVICE_REGISTERED, routing_key) == 0) {
+	case removed_evt:
+		removed_cb = handler->cb;
+		id = get_id_from_json_str(jso);
+		removed_cb(id, user_data);
+		break;
+	case added_evt:
+		added_cb = handler->cb;
+		id = get_id_from_json_str(jso);
 		token = get_token_from_jobj(jso);
 		if (!token)
 			return false;
 
-		cloud_cbs.added_cb(id, token, user_data);
+		added_cb(id, token, user_data);
+		break;
+	case listed_evt:
+		listed_cb = handler->cb;
+		list = parser_mydevices_to_list(body);
+		if (!l_queue_isempty(list))
+			listed_cb(list, user_data);
+
+		l_queue_destroy(list, NULL);
+		l_hashmap_remove(map_event_to_cb,
+					AMQP_EVENT_DEVICE_LIST);
+		l_free(handler);
+		break;
+	default:
+		hal_log_error("Unknown event %s", routing_key);
+		break;
 	}
 
-done:
 	json_object_put(jso);
 	return true;
 }
@@ -162,9 +182,14 @@ int cloud_list_devices(cloud_devices_cb_t on_listed)
 	amqp_bytes_t queue_cloud;
 	json_object *jobj_empty;
 	const char *json_str;
+	const struct event_handler handler = {
+		.cb = on_listed,
+		.event_id = listed_evt
+	};
 	int result;
 
-	cloud_cbs.listed_cb = on_listed;
+	l_hashmap_insert(map_event_to_cb, AMQP_EVENT_DEVICE_LIST,
+				l_memdup(&handler, sizeof(handler)));
 	jobj_empty = json_object_new_object();
 	json_str = json_object_to_json_string(jobj_empty);
 
@@ -290,14 +315,17 @@ int cloud_set_cbs(cloud_downstream_cb_t on_update,
 		AMQP_EVENT_DEVICE_LIST,
 		NULL
 	};
+	const struct event_handler handlers[] = {
+		{ .cb = on_update, .event_id = listed_evt },
+		{ .cb = on_request, .event_id = request_evt },
+		{ .cb = on_removed, .event_id = removed_evt },
+		{ .cb = on_added, .event_id = added_evt },
+		{ .cb = NULL, .event_id = listed_evt },
+	};
 	amqp_bytes_t queue_fog;
 	int err, i;
 
-
-	cloud_cbs.update_cb = on_update;
-	cloud_cbs.request_cb = on_request;
-	cloud_cbs.removed_cb = on_removed;
-	cloud_cbs.added_cb = on_added;
+	map_event_to_cb = l_hashmap_string_new();
 
 	queue_fog = amqp_declare_new_queue(AMQP_QUEUE_FOG);
 	if (queue_fog.bytes == NULL) {
@@ -306,6 +334,10 @@ int cloud_set_cbs(cloud_downstream_cb_t on_update,
 	}
 
 	for (i = 0; fog_events[i] != NULL; i++) {
+		if (handlers[i].cb)
+			l_hashmap_insert(map_event_to_cb, fog_events[i],
+					 l_memdup(handlers + i,
+						  sizeof(handlers[i])));
 		err = amqp_set_queue_to_consume(queue_fog, AMQP_EXCHANGE_FOG,
 					fog_events[i]);
 		if (err) {
@@ -332,5 +364,6 @@ int cloud_start(struct settings *settings)
 
 void cloud_stop(void)
 {
+	l_hashmap_destroy(map_event_to_cb, l_free);
 	amqp_stop();
 }
